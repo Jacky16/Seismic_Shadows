c5dc3f0445c150e0f0b9bf4c75f2dd1093c27c61
Adria <adriamartinez94@gmail.com>
2021-03-01 20:29:18 +0100

    Movimiento y salto simple

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] PlayerMovement player;
+    Vector2 axis;
+    public void OnMovement(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            axis = ctx.ReadValue<Vector2>();
+        }
+        else if (ctx.canceled)
+        {
+            axis = Vector2.zero;
+        }
+        player.SetAxis(axis);
+    }
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.action.triggered)
+        {
+            player.Jump();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Setting Player")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    [SerializeField] LayerMask layerMaskGround;
+    [SerializeField] Transform feetPos;
+    bool isGrounded;
+
+    //Componentes
+    Rigidbody2D rb2d;
+    //Vectores
+    Vector2 axis;
+    Vector2 movement;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, 0.2f, layerMaskGround);
+    }
+
+    private void Movement()
+    {
+        movement = new Vector2(axis.x * speed * 100, rb2d.velocity.y);
+        rb2d.velocity = movement;
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb2d.velocity = (Vector2.up * jumpForce * 100);
+        }
+    }
+    public void SetAxis(Vector2 _axis)
+    {
+        axis = _axis;
+    }
+}
