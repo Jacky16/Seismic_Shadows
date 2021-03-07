c44f26a9ecbc744ba50c63740bef3b15e60d5eb4
Adria Martinez <adriamartinez94@gmail.com>
2021-03-07 11:54:54 +0100

    Un pulido de las cosas preparadas de las trampas

---
+++ Assets/Scripts/Waves/Behaivour/BehaivourWave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaivourWave : MonoBehaviour
+{
+    protected Rigidbody2D rb2d;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    protected virtual void Update()
+    {
+
+    }
+
+    protected virtual void ActionWave(Collider2D col) {; }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ActionWave(collision);      
+    }
+}
