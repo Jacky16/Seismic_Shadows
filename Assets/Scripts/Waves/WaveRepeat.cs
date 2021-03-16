81d9ccf3348f8a7f9855e16b61c1330cd4c326a5
Adria Martinez <adriamartinez94@gmail.com>
2021-03-16 19:57:01 +0100

    Repetidor de ondas  y hecha bien la animación del "Pess space"

---
+++ Assets/Scripts/Waves/WaveRepeat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveRepeat : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<WaveAnimation>(out WaveAnimation wa))
+        {
+            GameObject go = Instantiate(collision.gameObject);
+            go.transform.position = transform.position;
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), go.GetComponent<Collider2D>());
+        }
+    }  
+}
