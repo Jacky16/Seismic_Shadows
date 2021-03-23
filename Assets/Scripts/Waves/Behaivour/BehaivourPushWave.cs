03d15480275104316ef7559bc5b7a542962bc007
Adria Martinez <adriamartinez94@gmail.com>
2021-03-23 18:24:31 +0100

    Puesto el comportamiento de las ondas

--- Assets/Scripts/Waves/Behaivour/BehaivourPushWave.cs
+++ Assets/Scripts/Waves/Behaivour/BehaivourPushWave.cs
@@ -10,11 +10,20 @@ public class BehaivourPushWave : BehaivourWave
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        rb2d.isKinematic = true;
+    }
+    private void Update()
+    {
+        if(rb2d.velocity == Vector2.zero)
+        {
+            rb2d.isKinematic = true;     
+        }
     }
     protected override void ActionOnWave(Collider2D col)
     {
         if (col.CompareTag("PushWave"))
         {
+            rb2d.isKinematic = false;
             Vector2 dir = transform.position - col.transform.position;
             rb2d.AddForce(dir.normalized * forcePush, ForceMode2D.Impulse);
         }
