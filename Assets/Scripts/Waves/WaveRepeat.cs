30a82b5be8416af638f0856f5107ff25ec580acb
Adria Martinez <adriamartinez94@gmail.com>
2021-03-29 13:48:39 +0200

    Adaptado el repetidor de ondas a las nuevas ondas

--- Assets/Scripts/Waves/WaveRepeat.cs
+++ Assets/Scripts/Waves/WaveRepeat.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class WaveRepeat : MonoBehaviour
 {
+    //Settings Waves
+    [Header("Step Wave")]
+    [SerializeField] GameObject stepWavePrefab;
+
+    [Header("Stealth Wave")]
+    [SerializeField] GameObject stealthWavePrefab;
+
+    [Header("Long Wave")]
+    [SerializeField] GameObject longWavePrefab;
+
+    [Header("Interactive Wave")]
+    [SerializeField] GameObject interactiveWavePrefab;
+
+    [Header("Push Wave")]
+    [SerializeField] GameObject pushWavePrefab;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.TryGetComponent<WaveAnimation>(out WaveAnimation wa))
+        string waveName = collision.tag;
+        Debug.Log(waveName);
+        switch (waveName)
         {
-            GameObject go = Instantiate(collision.gameObject);
-            go.transform.position = transform.position;
-            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), go.GetComponent<Collider2D>());
+            case "StepWave":
+                InstantiateWave(stepWavePrefab);
+                break;
+            case "StealthWave":
+                InstantiateWave(stealthWavePrefab);
+                break;
+            case "LongWave":
+                InstantiateWave(longWavePrefab);
+                break;
+            case "InteractiveWave":
+                InstantiateWave(interactiveWavePrefab);
+                break;
+            case "PushWave":
+                InstantiateWave(pushWavePrefab);
+                break;
+            default:
+                break;
         }
-    }  
+    }
+    void InstantiateWave(GameObject g)
+    {
+        GameObject go = Instantiate(g, transform.position, Quaternion.identity, null);
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), go.GetComponent<Collider2D>());
+        Destroy(go, 2);
+    }
 }
