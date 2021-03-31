1cd180b419ea2da585a2faa13dbe80c510c63859
Adria Martinez <adriamartinez94@gmail.com>
2021-03-31 19:15:53 +0200

    Hecho la lógica de los ajustes

--- Assets/Scripts/Managers/UI/OptionsManager.cs
+++ Assets/Scripts/Managers/UI/OptionsManager.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-
+using UnityEngine.Audio;
 
 public class OptionsManager : MonoBehaviour
 {
+    [Header("Canvas GameObjects")]
     [SerializeField] GameObject canvasBeforeOptions;
     [SerializeField] GameObject firstButtonSelected;
     EventSystem eventSystem;
+    [Header("Audio")]
+    [SerializeField] AudioMixer audiomixer;
+    [SerializeField] Slider sliderMusic;
+    [SerializeField] Slider sliderSounds;
+    float volumeMusic;
+    float volumeSounds;
+
+    [SerializeField] Toggle fullScreenToogle;
+    bool isFullScreen;
     private void Awake()
     {
         eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
-    }
-    private void Start()
-    {
-        gameObject.SetActive(false);
+        InitVolumeSettings();
+       
     }
     private void OnEnable()
     {
@@ -27,4 +35,30 @@ public class OptionsManager : MonoBehaviour
         canvasBeforeOptions.SetActive(true);
         gameObject.SetActive(false);
     }
+
+    public void OnChangeMusicVolume()
+    {
+        audiomixer.SetFloat("musicVolume", sliderMusic.value);
+        PlayerPrefs.SetFloat("musicVolume", sliderMusic.value);
+    }
+    public void OnChangeSoundVolume()
+    {
+        audiomixer.SetFloat("soundVolume", sliderSounds.value);
+        PlayerPrefs.SetFloat("soundVolume", sliderSounds.value);
+    }
+    void InitVolumeSettings()
+    {
+        //Music
+        volumeMusic = PlayerPrefs.GetFloat("musicVolume", 0);
+        sliderMusic.value = volumeMusic;
+        //Sounds
+        volumeSounds = PlayerPrefs.GetFloat("soundVolume", 0);
+        sliderSounds.value = volumeSounds;
+
+    }
+    public void FullScreen(bool _b)
+    {
+        Screen.fullScreen = _b;
+    }
+
 }
