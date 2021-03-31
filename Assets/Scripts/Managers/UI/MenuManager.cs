f22093c5c43e21bcfb2f495f25b163d2f9ed7ccf
Adria Martinez <adriamartinez94@gmail.com>
2021-03-31 14:47:15 +0200

    Haciendo menú principal y opciones

---
+++ Assets/Scripts/Managers/UI/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject firstButtonSelected;
+    [SerializeField] GameObject canvasMainMenu;
+    [SerializeField] GameObject canvasOptions;
+    [SerializeField]EventSystem eventSystem;
+    private void OnEnable()
+    {
+        canvasOptions.SetActive(false);
+        eventSystem.SetSelectedGameObject(firstButtonSelected);
+
+    }
+    public void ContinueGame()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("BlindurLevel");
+    }
+    public void Options()
+    {
+        canvasOptions.SetActive(true);
+        canvasMainMenu.SetActive(false);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Managers/UI/OptionsManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class OptionsManager : MonoBehaviour
+{
+    [SerializeField] GameObject canvasBeforeOptions;
+    [SerializeField] GameObject firstButtonSelected;
+    [SerializeField] EventSystem eventSystem;
+
+    private void OnEnable()
+    {
+        eventSystem.SetSelectedGameObject(firstButtonSelected);
+    }
+    public void Return()
+    {
+        canvasBeforeOptions.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
