d15f173ad6ca871fda0d4a998f134c69946e7f04
Adria Martinez <adriamartinez94@gmail.com>
2021-04-03 14:30:45 +0200

    Añadido que el primer botón de cada canvas sea seleccionado

---
+++ Assets/Scripts/Enemies/Managers/UI/FirstButtonSelected.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FirstButtonSelected : MonoBehaviour
+{
+    EventSystem eventSystem;
+    [SerializeField] GameObject firstGameObjectSelected;
+    private void Awake()
+    {
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        
+    }
+    private void OnEnable()
+    {
+        eventSystem.SetSelectedGameObject(firstGameObjectSelected);
+    }
+}
