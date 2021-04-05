26388e91eb1243f49b9f2c07cacdee886dd3bbba
Venti <diguitys225@gmail.com>
2021-04-05 18:03:16 +0200

    Arreglado movimiento del parallax

--- Assets/Scripts/Environment/Parallax.cs
+++ Assets/Scripts/Environment/Parallax.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    Rigidbody2D player;
+
     [SerializeField] float speed;
-    Rigidbody2D prb2d;
     Rigidbody2D backrb2d;
     Transform ppos;
+    InputManager input;
+    PlayerMovement pmov;
     
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        pmov = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        input = GameObject.FindGameObjectWithTag("InputManager").GetComponent<InputManager>();
         backrb2d = gameObject.GetComponent<Rigidbody2D>();
         ppos = GameObject.FindGameObjectWithTag("Player").transform;
     }
@@ -27,18 +29,17 @@ public class Parallax : MonoBehaviour
     {
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, ppos.position.y, gameObject.transform.position.z);
 
-        if (player.velocity.x > 0)
+        if(input.GetAxis().x == 1 && !pmov.GetWallPos())
         {
-            backrb2d.velocity = new Vector3(-speed, 0,0);
+            backrb2d.velocity = new Vector3(-speed, 0, 0);
         }
-        else if (player.velocity.x < 0)
+        if (input.GetAxis().x == -1 && !pmov.GetWallPos())
         {
             backrb2d.velocity = new Vector3(speed, 0, 0);
         }
-
-        if (player.velocity.x == 0)
+        if (input.GetAxis().x == 0 || pmov.GetWallPos())
         {
-            backrb2d.velocity = Vector3.zero;
+            backrb2d.velocity = new Vector3(0, 0, 0);
         }
     }
 }
