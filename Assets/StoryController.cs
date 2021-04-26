8c709a1ee40ba01915494e204026b9182593b8dc
Venti <diguitys225@gmail.com>
2021-04-26 19:18:36 +0200

    Añadidas escenas de la historia

--- Assets/StoryController.cs
+++ Assets/StoryController.cs
@@ -2,28 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class StoryController : MonoBehaviour
 {
+    [TextArea]
+    [SerializeField] string story;
+    [SerializeField] TextMeshProUGUI textToShow;
+    [SerializeField] string numberStory;
+    MusicController music;
+    int textLength;
+    string completeText = "";
+    string actualText = "";
     float counter;
+    int index;
+    bool completed;
     // Start is called before the first frame update
     void Start()
     {
+        index = 0;
+        textToShow.text = "";
+        textLength = story.Length;
         counter = 0;
+        completed = false;
+        music = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<MusicController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        
+        counter += Time.deltaTime;
+        if (index < textLength)
         {
-            SceneManager.LoadScene("1_UpperMantle");
+            if (counter >= 0.05)
+            {
+                actualText = actualText + story[index];
+                textToShow.text = actualText;
+                index++;
+                counter = 0;
+            }
         }
 
-        counter += Time.deltaTime;
-        if(counter >= 70)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene("1_UpperMantle");
+            if (completed)
+            {
+                if(SceneManager.GetActiveScene().name != "Story_8")
+                {
+                    SceneManager.LoadScene("Story_" + numberStory);
+                }
+
+                else
+                {
+                    music.destroyItem();
+                    SceneManager.LoadScene("1_UpperMantle");
+                }
+            }
+            else
+            {
+                for (int j = 0; j < textLength; j++)
+                {
+                    completeText = completeText + story[j];
+                }
+
+                textToShow.text = completeText;
+                index = textLength;
+            }
         }
+
+        if (index == textLength)
+        {
+            completed = true;
+        }
+
     }
 }
