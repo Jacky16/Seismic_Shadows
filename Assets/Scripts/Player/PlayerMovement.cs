00937e232bcfd05f311ef6a18d9e9c7d87d59980
Adria Martinez <adriamartinez94@gmail.com>
2021-04-29 17:37:37 +0200

    Arreglado layer del wall jumping

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -25,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Settings WallSliding")]
     [SerializeField] float wallSlideSpeed;
     [SerializeField] Transform wallCheckPoint;
+    [SerializeField] LayerMask wallLayer;
     bool isTouchingWall;
     bool isWallSliding;
 
@@ -171,7 +172,7 @@ public class PlayerMovement : MonoBehaviour
     void CheckWorld()
     {
         grounded = Physics2D.OverlapBox(groundCheckPoint.position, groundCheckSize, 0, groundLayer);
-        isTouchingWall = Physics2D.Raycast(wallCheckPoint.position, wallCheckPoint.up, wallCheckDistance, groundLayer);
+        isTouchingWall = Physics2D.Raycast(wallCheckPoint.position, wallCheckPoint.up, wallCheckDistance, wallLayer);
         anim.SetBool(IDIsGroundesParam, grounded);
         anim.SetBool("IsSliding", isTouchingWall);
     }
