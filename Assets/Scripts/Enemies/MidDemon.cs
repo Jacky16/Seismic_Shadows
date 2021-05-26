aae508ee0a82dfe684a4083d152d646478d9a15b
Venti <diguitys225@gmail.com>
2021-05-26 12:15:35 +0200

    Subido spritesheet bicho que invoca el boss

---
+++ Assets/Scripts/Enemies/MidDemon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidDemon : Enemy
+{
+    GameObject player;
+    HealthPlayer hpp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        hpp = player.GetComponent<HealthPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = (target.position - transform.position).normalized;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Stalactite")
+        {
+            healthEnemie.Damage(1);
+        }
+        if (collision.collider.tag == "Player")
+        {
+            hpp.Damage(1);
+        }
+    }
+}
