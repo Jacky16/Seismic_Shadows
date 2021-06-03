f601a9c23ab40dfb3782820edcf099d288299380
Adria Martinez <adriamartinez94@gmail.com>
2021-06-03 17:06:20 +0200

    Arreglado el stalactite spawner

--- Assets/Scripts/Environment/StalactiteSpawner.cs
+++ Assets/Scripts/Environment/StalactiteSpawner.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class StalactiteSpawner : MonoBehaviour
 {
+    [SerializeField] float timeToSpawn;
     [SerializeField] GameObject stalactitePrefab;
 
-    public void InstantiateStalactite()
+    public void InstantiateStalactite(Vector2 _pos)
     {
-        Instantiate(stalactitePrefab, transform.position, Quaternion.identity, null);
+        StartCoroutine(DelaySpawn(_pos));
+    }
+    IEnumerator DelaySpawn(Vector2 _pos)
+    {
+        yield return new WaitForSeconds(timeToSpawn);
+        Instantiate(stalactitePrefab, _pos, Quaternion.identity, null);
+
     }
 }
